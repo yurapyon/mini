8f630bd6581ee12e0a39f2f04e38ce4a6f5a15a0
yurapyon <genericpb@gmail.com>
2024-07-20 23:00:53 -0700

    start zig implementation

---
+++ zig/src/main.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const vm = @import("vm.zig");
+
+fn runMiniVM(allocator: Allocator) !void {
+    var VM: vm.MiniVM = undefined;
+    try VM.init(allocator);
+}
+
+pub fn main() !void {}
+
+test "simple test" {
+    try runMiniVM(std.testing.allocator);
+}
