2dbdcf539302630d92e68dfd27a4dc6445efd1bc
yurapyon <genericpb@gmail.com>
2024-07-31 01:58:18 -0700

    system

--- zig/src/devices/System.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-const vm = @import("../mini.zig");
-
-pub const System = struct {
-    pub fn onActivate(_: @This(), _: *vm.MiniVM) void {
-        std.debug.print("activate system\n", .{});
-    }
-};
---
+++ zig/src/system.zig
@@ -0,0 +1,10 @@
+pub const System = struct {
+    pub fn init() !void {
+        // init memory
+        // create the vm
+        // spawn vm thread
+    }
+    pub fn mainLoop() !void {}
+};
+
+pub fn runVM() !void {}
