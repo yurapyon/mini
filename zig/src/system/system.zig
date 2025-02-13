d18b32f4ecdc7c1905827b1bc9b4b79948c7066c
yurapyon <genericpb@gmail.com>
2025-02-13 11:20:48 -0600

    stuff

--- zig/src/system/system.zig
+++ zig/src/system/system.zig
@@ -9,35 +9,105 @@ const externals = @import("../externals.zig");
 const External = externals.External;
 
 const c = @import("c.zig");
-const Video = @import("video.zig").Video;
+
+const video = @import("video.zig");
+const Video = video.Video;
 
 // ===
 
+const window_title = "pyon vPC";
+
 const ExternalId = enum(Cell) {
     bye = 64,
-    putc,
-    diskStore,
-    diskFetch,
+    pixel,
+    palette,
     _,
 };
 
 const system_file = @embedFile("system.mini.fth");
 
 fn externalsCallback(rt: *Runtime, token: Cell, userdata: ?*anyopaque) External.Error!bool {
-    _ = rt;
-    _ = token;
-    _ = userdata;
+    const system = @as(*System, @ptrCast(@alignCast(userdata)));
+
+    const external_id = @as(ExternalId, @enumFromInt(token));
+
+    switch (external_id) {
+        .bye => {
+            // TODO
+            // maybe just trigger glfw window close
+        },
+        .pixel => {
+            const page = rt.data_stack.pop();
+            const addr = rt.data_stack.pop();
+            const color = rt.data_stack.pop();
+
+            system.video.putPixel(
+                page,
+                addr,
+                @truncate(color),
+            );
+        },
+        .palette => {
+            const at = rt.data_stack.pop();
+            const b = rt.data_stack.pop();
+            const g = rt.data_stack.pop();
+            const r = rt.data_stack.pop();
+
+            system.video.setPalette(
+                @truncate(at),
+                @truncate(r),
+                @truncate(g),
+                @truncate(b),
+            );
+        },
+        else => {},
+    }
 }
 
 pub const System = struct {
     window: *c.GLFWwindow,
 
+    // TODO
+    // should_bye: bool,
+
     // devices
     video: Video,
 
     pub fn init(self: *@This(), rt: *Runtime) !void {
-        self.window = try c.gfx.init();
+        if (c.glfwInit() != c.GL_TRUE) {
+            return error.CannotInitGLFW;
+        }
+        errdefer c.glfwTerminate();
+
+        c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, @intCast(3));
+        c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, @intCast(3));
+        c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+        c.glfwWindowHint(c.GLFW_RESIZABLE, c.GL_FALSE);
+        c.glfwWindowHint(c.GLFW_FLOATING, c.GL_TRUE);
+        c.glfwSwapInterval(1);
+
+        // note: window creation fails if we can't get the desired opengl version
+
+        const window = c.glfwCreateWindow(
+            video.width * 2,
+            video.height * 2,
+            window_title,
+            null,
+            null,
+        ) orelse return error.CannotInitWindow;
+        errdefer c.glfwDestroyWindow(window);
+
+        c.glfwMakeContextCurrent(window);
+
+        var w: c_int = undefined;
+        var h: c_int = undefined;
+        c.glfwGetFramebufferSize(window, &w, &h);
+        c.glViewport(0, 0, w, h);
+
+        self.window = window;
+
         self.video.init();
+
         rt.processBuffer(system_file) catch |err| switch (err) {
             error.WordNotFound => {
                 std.debug.print("Word not found: {s}\n", .{
@@ -51,14 +121,21 @@ pub const System = struct {
     }
 
     pub fn deinit(_: @This()) void {
-        c.gfx.deinit();
+        c.glfwTerminate();
     }
 
     pub fn loop(self: *@This()) !void {
         while (c.glfwWindowShouldClose(self.window) == c.GL_FALSE) {
             c.glClear(c.GL_COLOR_BUFFER_BIT);
+            self.video.draw();
             c.glfwSwapBuffers(self.window);
+
             c.glfwPollEvents();
+
+            // TODO
+            // std.time.sleep(33000);
         }
+
+        self.deinit();
     }
 };
--- zig/src/system/video.zig
+++ zig/src/system/video.zig
@@ -1,70 +1,226 @@
 const c = @import("c.zig");
-const gfx = c.gfx;
 
 const runtime = @import("../runtime.zig");
 const Cell = runtime.Cell;
 
-// 5x7 characters, drawn with 1px right and bottom border
-const Character = [5]u8;
+// ===
+
+// 64k * 3 can max 512 x 384 x 8bit
+const page_size = 64 * 1024;
+const page_ct = 3;
+
+pub const width = 512;
+pub const height = 384;
+
 // 256 color palette with 24bit color
 const RGB = [3]u8;
 
 pub const Video = struct {
-    // 64k * 3 can max 512 x 384 x 8bit
-    // This could fit a 400x300 drawing canvas
-    // For 6x8 characters, this can fit 85x48, or 4080 chars total
-    buffer: [64 * 1024 * 3]u8,
-    character_map: [256]Character,
+    buffer: [page_size * page_ct]RGB,
     palette: [256]RGB,
 
     texture: c.GLuint,
-    quad: c.GLuint,
+    vbo: c.GLuint,
     vao: c.GLuint,
     prog: c.GLuint,
 
     pub fn init(self: *@This()) void {
-        self.texture = gfx.texture.createBlank(512, 384);
-        self.quad = gfx.buffer.createQuad();
-        self.prog = gfx.program.create();
+        self.makeTexture();
+        self.makeQuad();
+        self.makeProgram();
         // TODO set uniforms
-
     }
 
-    pub fn putCharacter(self: *@This(), x: Cell, y: Cell, char: u8) void {
+    pub fn deinit(_: *@This()) void {
+        // TODO
+    }
+
+    // ===
+
+    fn makeTexture(self: *@This()) void {
+        var tex: c.GLuint = undefined;
+        c.glGenTextures(1, &tex);
+        c.glBindTexture(c.GL_TEXTURE_2D, tex);
+
+        c.glTexImage2D(
+            c.GL_TEXTURE_2D,
+            0,
+            c.GL_RGB,
+            width,
+            height,
+            0,
+            c.GL_RGB,
+            c.GL_UNSIGNED_BYTE,
+            null,
+        );
+
+        // c.glGenerateMipmap(c.GL_TEXTURE_2D);
+        c.glTexParameteri(
+            c.GL_TEXTURE_2D,
+            c.GL_TEXTURE_WRAP_S,
+            c.GL_REPEAT,
+        );
+        c.glTexParameteri(
+            c.GL_TEXTURE_2D,
+            c.GL_TEXTURE_WRAP_T,
+            c.GL_REPEAT,
+        );
+        c.glTexParameteri(
+            c.GL_TEXTURE_2D,
+            c.GL_TEXTURE_MIN_FILTER,
+            c.GL_NEAREST,
+        );
+        c.glTexParameteri(
+            c.GL_TEXTURE_2D,
+            c.GL_TEXTURE_MAG_FILTER,
+            c.GL_NEAREST,
+        );
+
+        c.glBindTexture(c.GL_TEXTURE_2D, 0);
+
+        self.texture = tex;
+    }
+
+    fn makeQuad(self: *@This()) void {
+        var vbo: c.GLuint = undefined;
+        c.glGenBuffers(1, &vbo);
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, vbo);
+        const data = [_]f32{
+            1.0, 1.0, 1.0, 1.0,
+            0.0, 1.0, 0.0, 1.0,
+            1.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 0.0,
+        };
+        c.glBufferData(
+            c.GL_ARRAY_BUFFER,
+            @sizeOf(@TypeOf(data)),
+            &data,
+            c.GL_STATIC_DRAW,
+        );
+        self.vbo = vbo;
+    }
+
+    fn makeProgram(self: *@This()) void {
         _ = self;
-        _ = x;
-        _ = y;
-        _ = char;
     }
 
-    pub fn putPixel(self: *@This(), x: Cell, y: Cell, color: u8) void {
-        _ = self;
-        _ = x;
-        _ = y;
-        _ = color;
-    }
+    // ===
 
-    pub fn blit(
+    pub fn putPixel(
         self: *@This(),
-        x: Cell,
-        y: Cell,
-        w: Cell,
-        h: Cell,
-        colors: []u8,
-        mask: []u8,
+        page: Cell,
+        addr: Cell,
+        palette_idx: u8,
     ) void {
-        _ = self;
-        _ = x;
-        _ = y;
-        _ = w;
-        _ = h;
-        _ = colors;
-        _ = mask;
+        const color = self.palette[palette_idx];
+        const page_at = page % page_ct;
+        const buffer_color = self.buffer[page_at * page_size + addr][0..2];
+        @memcpy(buffer_color, color);
     }
 
-    pub fn swapBuffers(self: *@This()) void {
+    pub fn setPalette(self: *@This(), at: u8, r: u8, g: u8, b: u8) void {
+        self.palette[at][0] = r;
+        self.palette[at][1] = g;
+        self.palette[at][2] = b;
+    }
+
+    pub fn draw(self: *@This()) void {
         _ = self;
         // copy buffer to texture
         // show texture
     }
+};
+
+const gfx = struct {
+    const vert_shader = @embedFile("shaders/vert.glsl");
+    const frag_shader = @embedFile("shaders/frag.glsl");
+
+    pub const shader = struct {
+        pub fn create(str: []const u8, shader_type: c.GLenum) c.GLuint {
+            const shd = c.glCreateShader(shader_type);
+            if (shd == 0) {
+                // TODO error
+                return 0;
+            }
+
+            c.glShaderSource(
+                shd,
+                1,
+                &[_]*const u8{@ptrCast(str.ptr)},
+                &[_]c_int{@intCast(str.len)},
+            );
+            c.glCompileShader(shd);
+
+            var info_len: c_int = 0;
+            c.glGetShaderiv(shd, c.GL_INFO_LOG_LENGTH, &info_len);
+            if (info_len != 0) {
+                // TODO error
+                // var buf = try heap_alloc.alloc(u8, @intCast(usize, info_len));
+                // glGetShaderInfoLog(shader, info_len, null, buf.ptr);
+                // std.debug.print("shader info:\n{s}", .{buf});
+                // heap_alloc.free(buf);
+            }
+
+            var success: c_int = undefined;
+            c.glGetShaderiv(shd, c.GL_COMPILE_STATUS, &success);
+            if (success != c.GL_TRUE) {
+                // TODO error
+                return 0;
+            }
+            return shd;
+        }
+
+        pub fn deinit(shd: c.GLuint) void {
+            // TODO
+            _ = shd;
+        }
+    };
+
+    pub const program = struct {
+        pub fn create() c.GLuint {
+            const vert = shader.create(vert_shader, c.GL_VERTEX_SHADER);
+            const frag = shader.create(frag_shader, c.GL_FRAGMENT_SHADER);
+            const shaders = [_]c.GLuint{
+                vert,
+                frag,
+            };
+            const prog = c.glCreateProgram();
+            errdefer c.glDeleteProgram(prog);
+
+            if (prog == 0) {
+                // TODO handle error
+                return 0;
+            }
+
+            for (shaders) |shd| {
+                c.glAttachShader(prog, shd);
+            }
+
+            c.glLinkProgram(prog);
+
+            var info_len: c_int = 0;
+            c.glGetProgramiv(prog, c.GL_INFO_LOG_LENGTH, &info_len);
+            if (info_len != 0) {
+                // TODO handle error
+                // var buf = try heap_alloc.alloc(u8, @intCast(usize, info_len));
+                // glGetProgramInfoLog(program, info_len, null, buf.ptr);
+                // std.debug.print("program info:\n{s}", .{buf});
+                // heap_alloc.free(buf);
+            }
+
+            var success: c_int = undefined;
+            c.glGetProgramiv(prog, c.GL_LINK_STATUS, &success);
+            if (success != c.GL_TRUE) {
+                // TODO handle error
+                return 0;
+            }
+
+            return prog;
+        }
+
+        pub fn deinit(prog: c.GLuint) void {
+            // TODO
+            _ = prog;
+        }
+    };
 };
