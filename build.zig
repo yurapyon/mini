87625bbd2448980e380fa2eec7b90dcfa152a97b
parcel <genericpb@gmail.com>
2026-01-20 02:00:42 -0600

    stuff

--- build.zig
+++ build.zig
@@ -105,7 +105,7 @@ pub fn build(b: *std.Build) void {
         .flags = &[_][]const u8{"-std=c99"},
     });
 
-    const exe = b.addExecutable(.{
+    const desktop = b.addExecutable(.{
         .name = "mini",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
@@ -119,6 +119,21 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
+    const desktop_install = b.addInstallArtifact(desktop, .{});
+
+    const desktop_step = b.step("desktop", "Build and install desktop");
+    desktop_step.dependOn(&desktop.step);
+
+    const run_cmd = b.addRunArtifact(desktop);
+    run_cmd.step.dependOn(&desktop_install.step);
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
     // ===
 
     const wasm = setupWasm(b, mod_mini);
@@ -128,38 +143,8 @@ pub fn build(b: *std.Build) void {
 
     // ===
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // TODO make build all the default
-    const all_step = b.step("all", "Build and install everything");
-    all_step.dependOn(b.getInstallStep());
-    all_step.dependOn(wasm);
+    b.getInstallStep().dependOn(&desktop_install.step);
+    b.getInstallStep().dependOn(wasm);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -172,27 +157,28 @@ pub fn build(b: *std.Build) void {
     // Creates an executable that will run `test` blocks from the provided module.
     // Here `mod` needs to define a target, which is why earlier we made sure to
     // set the releative field.
-    const mod_tests = b.addTest(.{
-        .root_module = mod_mini,
-    });
+    // TODO
+    // const mod_tests = b.addTest(.{
+    // .root_module = mod_mini,
+    // });
 
     // A run step that will run the test executable.
-    const run_mod_tests = b.addRunArtifact(mod_tests);
+    // const run_mod_tests = b.addRunArtifact(mod_tests);
 
     // Creates an executable that will run `test` blocks from the executable's
     // root module. Note that test executables only test one module at a time,
     // hence why we have to create two separate ones.
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
+    // const exe_tests = b.addTest(.{
+    // .root_module = exe.root_module,
+    // });
 
     // A run step that will run the second test executable.
-    const run_exe_tests = b.addRunArtifact(exe_tests);
+    // const run_exe_tests = b.addRunArtifact(exe_tests);
 
     // A top level step for running all tests. dependOn can be called multiple
     // times and since the two run steps do not depend on one another, this will
     // make the two of them run in parallel.
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    // const test_step = b.step("test", "Run tests");
+    // test_step.dependOn(&run_mod_tests.step);
+    // test_step.dependOn(&run_exe_tests.step);
 }
